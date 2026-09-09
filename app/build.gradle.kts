c2f6a68dafcd4434450a5410976509fdef61f382
vampiremystery100-boop <vampire.mystery100@gmail.com>
2026-09-09 16:15:04 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,15 @@ android {
         versionName = "1.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures {
         compose = true
     }
@@ -28,17 +37,15 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
 
     implementation("androidx.media3:media3-exoplayer:1.6.1")
-implementation("androidx.media3:media3-ui:1.6.1")
-implementation("androidx.media3:media3-common:1.6.1")
+    implementation("androidx.media3:media3-ui:1.6.1")
+    implementation("androidx.media3:media3-common:1.6.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
 }
